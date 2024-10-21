be224cd1b72a3e04c121df84847ab1914b8e9743
bloodking1904 <macielsevero19@gmail.com>
2024-10-21 16:57:18 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-
 const users = {
     admin: '1234',
     reginaldo: '1234',
@@ -14,6 +13,16 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const username = document.getElementById('username').value.toLowerCase();
     const password = document.getElementById('password').value;
 
+    // Verifica se o usuário é admin
+    if (username === 'admin') {
+        // Verifica a orientação da tela
+        if (window.innerHeight > window.innerWidth) {
+            alert("Por favor, mude seu dispositivo para o modo paisagem para fazer login como admin.");
+            return; // Impede o login se não estiver em modo paisagem
+        }
+    }
+
+    // Verifica as credenciais do usuário
     if (users[username] && users[username] === password) {
         localStorage.setItem('loggedInUser', username);
         window.location.href = 'index.html';
