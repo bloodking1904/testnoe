dc3991a400da47b4bf25a0f324bdb025840c043d
bloodking1904 <macielsevero19@gmail.com>
2025-01-15 14:23:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -252,7 +252,7 @@ async function verificarSemanaPassada() {
         await atualizarDadosDasSemanas(); // Aguarda a conclusão da atualização
 
         // Após a atualização, chama carregarMotoristas
-        await carregarMotoristas(); // Carrega os motoristas após a atualização
+    //    await carregarMotoristas(); // Carrega os motoristas após a atualização
         await verificarData(); // Verifica e atualiza a data se necessário
     } else {
         console.log("Ainda está na mesma semana.");
@@ -909,7 +909,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log("DOM totalmente carregado. Inicializando motoristas...");
     await verificarSemanaPassada(); // Chama a verificação de semana passada
 
-    carregarMotoristas().catch(console.error); // Chamada assíncrona
+    await carregarMotoristas().catch(console.error); // Chamada assíncrona
 
     // Eventos para os botões de navegação
     document.getElementById('seta-esquerda').addEventListener('click', () => {
